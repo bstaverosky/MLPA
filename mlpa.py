a1c5a6284c5a50b53e4a9ce1e359f633c1dd4367
Brian Staverosky <bstaverosky@gmail.com>
2023-06-11 13:11:32 -0400

    laptop 061123

--- mlpa.py
+++ mlpa.py
@@ -15,6 +15,10 @@ import yfinance as yf
 #import cvxpy as cp
 import statsmodels.api as sm
 import talib as talib
+from sklearn.ensemble import GradientBoostingRegressor
+import pyfolio as pf
+from datetime import datetime
+
 
 
 # Verify the versions of the loaded libraries (optional)
@@ -60,7 +64,7 @@ data_files = [
 
 dfs_to_merge = []
 
-file_path = "/home/bstaverosky/Documents/projects/MLPA/infl.csv"
+#file_path = "/home/bstaverosky/Documents/projects/MLPA/infl.csv"
 
 for file_path, var_name in data_files:
     df = pd.read_csv(file_path, skiprows=3, header=1, index_col="Date")
@@ -75,46 +79,53 @@ for df in dfs_to_merge:
     merged_df = merged_df.merge(df, left_index=True, right_index=True, how='outer')
 
 merged_df = merged_df.fillna(method = "ffill")
+#merged_df = merged_df.dropna()
 
+asset = merged_df
 
 ##### PARAMETERS #####
 algos = ["xg"]
 
-numest = [2,3,5,10]
-#nest = 10
-maxdepth = [1,2,3,5]
-#mdepth = 2
+numest = [5]
+nest = 10
+maxdepth = [5]
+mdepth = 5
 #prediction window
-predwindow = [3,5,10,21,63,126,256]
+predwindow = [21]
 #pw = 10
 #lookback window
-lw = 256
+lw = 2560
 # ticker
-ticker = "SPY"
+#ticker = "SPY"
 alg = "xg"
 
 
-
-
-for pw in predwindow:
-    for nest in numest:
-        for mdepth in maxdepth:
-            asset = yf.download(ticker, start='1900-01-01', progress=True)
+#for pw in predwindow:
+#    for nest in numest:
+#        for mdepth in maxdepth:
             
-            # Calculate signals
-            # SMA RATIO
-            asset['sma_rat'] = np.log(talib.SMA(asset['Close'], timeperiod=21)/talib.SMA(asset['Close'], timeperiod = 252))
+           
+            pw = 21
+            lw = 2560
             
-            # VOL RATIO
-            for i in range(len(asset.index)):
-                asset.loc[asset.index[i], "stvol"] = np.std(np.diff(np.log(asset.loc[asset.index[1:i], "Close"].tail(65))))
-                asset.loc[asset.index[i], "ltvol"] = np.std(np.diff(np.log(asset.loc[asset.index[1:i], "Close"].tail(252))))
-                asset.loc[asset.index[i], "vol_rat"] = asset.loc[asset.index[i], "stvol"]/asset.loc[asset.index[i], "ltvol"]
+            {
+            
+            # asset = yf.download(ticker, start='1900-01-01', progress=True)
+            
+            # # Calculate signals
+            # # SMA RATIO
+            # asset['sma_rat'] = np.log(talib.SMA(asset['Close'], timeperiod=21)/talib.SMA(asset['Close'], timeperiod = 252))
+            
+            # # VOL RATIO
+            # for i in range(len(asset.index)):
+            #     asset.loc[asset.index[i], "stvol"] = np.std(np.diff(np.log(asset.loc[asset.index[1:i], "Close"].tail(65))))
+            #     asset.loc[asset.index[i], "ltvol"] = np.std(np.diff(np.log(asset.loc[asset.index[1:i], "Close"].tail(252))))
+            #     asset.loc[asset.index[i], "vol_rat"] = asset.loc[asset.index[i], "stvol"]/asset.loc[asset.index[i], "ltvol"]
                 
-            # PRICE TO HIGH
-            for i in range(len(asset.index)):
-                asset.loc[asset.index[i], "p2h"] = asset.loc[asset.index[i], "Close"]/np.max(asset.loc[asset.index[(i-252):(i-1)], "Close"])
-                
+            # # PRICE TO HIGH
+            # for i in range(len(asset.index)):
+            #     asset.loc[asset.index[i], "p2h"] = asset.loc[asset.index[i], "Close"]/np.max(asset.loc[asset.index[(i-252):(i-1)], "Close"])
+            }    
             # Get Daily Return
             asset['dayret'] = asset['Close'].pct_change()
             
@@ -124,14 +135,20 @@ for pw in predwindow:
             def percentage_change(col1,col2):
                 return ((col2 - col1) / col1) * 100
             
-            asset['weekret'] = percentage_change(asset['closelag'],asset['Close'])
-            asset['weekret'] = asset['weekret'].shift(-(pw+1))
+            asset['pwret'] = percentage_change(asset['closelag'],asset['Close'])
+            # Shift so that your prediction period aligns with the current day plus
+            # one additional day to fix look ahead bias
+            asset['pwret'] = asset['pwret'].shift(-(pw+1))
+    
             
             # CLEAN DATAFRAME
             
-            df = asset[['sma_rat', 'vol_rat', 'p2h', 'weekret']].tail(len(asset)-lw)
+            #df = asset[['sma_rat', 'vol_rat', 'p2h', 'infl', 'ey', 'dy', 'aaa', 'tr', 'pb','ts', 'pwret']].tail(len(asset)-lw)
+            df = asset[['sma_rat', 'vol_rat', 'p2h', 'infl', 'ey', 'dy', 'ts', 'pwret']].tail(len(asset)-lw)
+            df = df.dropna()
             #df = df.dropna()
-            
+            #features = ['sma_rat', 'vol_rat', 'p2h', 'infl', 'ey', 'dy', 'aaa', 'tr', 'pb','ts']
+            features = ['sma_rat', 'vol_rat', 'p2h', 'infl', 'ey', 'dy', 'ts']
             
             predf = pd.DataFrame(columns = ["pred"])
             
@@ -148,12 +165,12 @@ for pw in predwindow:
                     model = LinearRegression()
                 
                 # Make trainsets
-                xtrain = df.loc[df.index[i-(lw+pw)]:df.index[i-pw],['sma_rat', 'vol_rat', 'p2h']]
-                ytrain = df.loc[df.index[i-(lw+pw)]:df.index[i-pw],['weekret']]
+                xtrain = df.loc[df.index[i-(lw+pw)]:df.index[i-pw],features]
+                ytrain = df.loc[df.index[i-(lw+pw)]:df.index[i-pw],['pwret']]
                     
                 # Make testsets
-                xtest = df.loc[[df.index[i+1]],['sma_rat', 'vol_rat', 'p2h']]    
-                ytest = df.loc[[df.index[i+1]],['weekret']]
+                xtest = df.loc[[df.index[i+1]],features]    
+                ytest = df.loc[[df.index[i+1]],['pwret']]
                 type(ytest)
                     
                 model.fit(xtrain, ytrain)
@@ -177,23 +194,23 @@ for pw in predwindow:
             # Create the strategy return performance
             for i in range(len(sframe.index)):
                 if sframe.loc[sframe.index[i], "signal"] > 0:
-                    sframe.loc[sframe.index[i], "strat"] = sframe.loc[sframe.index[i], "return"]*1.25
-                else:
                     sframe.loc[sframe.index[i], "strat"] = sframe.loc[sframe.index[i], "return"]*0.75
+                else:
+                    sframe.loc[sframe.index[i], "strat"] = sframe.loc[sframe.index[i], "return"]*-0.10
                     
             bmk_series = sframe.loc[:,"return"].tail(len(sframe)-(lw+pw))
             strat_series = sframe.loc[:,"strat"].tail(len(sframe)-(lw+pw))
             
             #tsheet = pf.create_simple_tear_sheet(returns = strat_series, benchmark_rets=bmk_series)
             
-            #pf.create_simple_tear_sheet(returns = strat_series, benchmark_rets=bmk_series)
+            pf.create_simple_tear_sheet(returns = strat_series, benchmark_rets=bmk_series)
             
             # Real-time forward week prediction
             rtpred = asset.loc[[asset.index[len(asset)-1]], ['sma_rat', 'vol_rat', 'p2h']]
             model.predict(rtpred)
             sframe = sframe.dropna()
             # Evaluate the test set RMSE
-            #MSE(sframe.loc[:,"weekret"], sframe.loc[:,"signal"])**(1/2)
+            #MSE(sframe.loc[:,"pwret"], sframe.loc[:,"signal"])**(1/2)
             
             
             # Performance Data Frame
@@ -213,17 +230,17 @@ for pw in predwindow:
                 'Benchmark Sharpe Ratio': ep.sharpe_ratio(bmk_series),
                 'Sortino Ratio': ep.sortino_ratio(strat_series),
                 'Max Drawdown': ep.max_drawdown(strat_series),
-                'Mean Squared Error': MSE(sframe.loc[:,"weekret"], sframe.loc[:,"signal"])**(1/2)
-                #'Baseline': (sframe.loc[:,"weekret_bin"] == 1).sum()/len(sframe),
-                #'Accuracy': accuracy_score(sframe.loc[:,"weekret_bin"], sframe.loc[:,"signal_bin"]),
-                #'Skill': accuracy_score(sframe.loc[:,"weekret_bin"], sframe.loc[:,"signal_bin"])-(sframe.loc[:,"weekret_bin"] == 1).sum()/len(sframe)
+                'Mean Squared Error': MSE(sframe.loc[:,"pwret"], sframe.loc[:,"signal"])**(1/2)
+                #'Baseline': (sframe.loc[:,"pwret_bin"] == 1).sum()/len(sframe),
+                #'Accuracy': accuracy_score(sframe.loc[:,"pwret_bin"], sframe.loc[:,"signal_bin"]),
+                #'Skill': accuracy_score(sframe.loc[:,"pwret_bin"], sframe.loc[:,"signal_bin"])-(sframe.loc[:,"pwret_bin"] == 1).sum()/len(sframe)
             })
 
             # Save to CSV
-            if path.exists('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv") == True:
-                perf.to_csv('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv", mode = 'a', header = False)
-            elif path.exists('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv") == False:
-                perf.to_csv('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv", header = True)
+            #if path.exists('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv") == True:
+             #   perf.to_csv('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv", mode = 'a', header = False)
+            #elif path.exists('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv") == False:
+             #   perf.to_csv('/home/brian/Documents/projects/shared_projects/Data' + "/" + "adhoc" + "_model_rolling_strat.csv", header = True)
 
 
     
